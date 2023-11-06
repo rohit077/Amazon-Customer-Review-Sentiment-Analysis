517012b0953c4e0988edb0ebad604145aa18776a
Rohit Roy Chowdhury <40126851+rohit077@users.noreply.github.com>
2023-11-06 11:49:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openai
 import pandas as pd
 
 # Set your OpenAI API key here
-os.environ['OPENAI_API_KEY'] = "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"
+os.environ['OPENAI_API_KEY'] = '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'
 
 st.title("Amazon Review Sentiment Analysis")
 #st.config(layout=wide)
